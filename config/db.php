f2e7b5f13c667903f373a5e432f2ca10771781a9
LiDmitry <owey@bk.ru>
2019-12-23 22:38:04 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=alt',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=us-cdbr-iron-east-05.cleardb.net;dbname=heroku_df769ea0191462e',
+    'username' => 'bcfc81e0e0b820',
+    'password' => '91e829b2',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
