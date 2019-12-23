631ad4f4b19dcfb843685227125f30bbe8af0c42
LiDmitry <owey@bk.ru>
2019-12-23 23:25:42 +0300

    6

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -156,7 +156,7 @@ class SiteController extends Controller
         if($model->load(\Yii::$app->request->post()) && $model->validate()){
             $user = new User();
             $user->username = $model->username;
-            $user->password = $model->\Yii::$app->security->generatePasswordHash($model->password);
+            $user->password = \Yii::$app->security->generatePasswordHash($model->password);
             if($user->save()){
 
                 return $this->goHome();
