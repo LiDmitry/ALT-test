748146f6feea664a958106abd2e88947be186e46
LiDmitry <owey@bk.ru>
2019-12-23 13:05:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,5 @@ defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
 
-$config = require __DIR__ . '/../config/web.php';
 
 (new yii\web\Application($config))->run();
