806d27fea2abb71739da12d8fb549020731ef703
LiDmitry <owey@bk.ru>
2019-12-24 13:30:42 +0300

    Update login.php

--- views/site/login.php
+++ views/site/login.php
@@ -40,7 +40,7 @@ $this->title = 'Авторизация';
             'labelOptions' => ['class' => 'col-lg-1 control-label'],
 
         ],
-        'action'=>'/web/index.php'
+        'action'=>'site/index'
     ]); ?>
 
         <?= $form->field($model, 'username' ,[
